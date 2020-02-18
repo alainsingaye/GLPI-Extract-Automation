c9fbed8fc82b5370caaa269231b13b637227572c
alainsingaye <60143615+alainsingaye@users.noreply.github.com>
2020-02-18 22:23:33 +0100

    Update Export.py

--- Export.py
+++ Export.py
@@ -1,9 +1,16 @@
-# Script permettant de Faire un Export de la base de GLPI des ordinateurs. Par Alain Singaye le 21/01/20 version 2.7
+# Script permettant de Faire un Export de la base de GLPI des ordinateurs. Par Alain Singaye le 18/02/20 version 2.8
 
 from selenium import webdriver
 
+#permet l'envoi de mail en utilisant le smpt
+import smtplib, ssl
+
+from email.message import EmailMessage
+
+#Pour mettre du temps entre les executation des tâches
 import time
 
+#Pour la création des loggins
 import logging
 
 #Pour la rotation des Logs
@@ -14,6 +21,11 @@ from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
 def access_to_website(url):
     chrome.get(url)
 
+#Pour envoyer un mail après l'export
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 
@@ -21,7 +33,7 @@ def access_to_website(url):
 # Permet d'enregistrer les logs dans un fichier et de faire La rotation
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-rotation_handler = TimedRotatingFileHandler(filename=r"C://your//directory//path//", when='H', interval=3)
+rotation_handler = TimedRotatingFileHandler(filename=r"C://Users//alain.singaye//Documents//GLPI//Logs//log.txt", when='M', interval=3)
 rotation_handler.setFormatter(formatter)
 logger = logging.getLogger()
 logger.addHandler(rotation_handler)
@@ -37,6 +49,7 @@ preferences = {"download.default_directory": download_dir ,
                "safebrowsing.enabled": True,
                "plugins.always_open_pdf_externally": True
                     }
+
 chrome_options.add_experimental_option("prefs", preferences)
 chrome = webdriver.Chrome(path, chrome_options=chrome_options)
 
@@ -45,7 +58,7 @@ chrome = webdriver.Chrome(path, chrome_options=chrome_options)
 #Try et Except permet de recouvrir des erreurs
 try:
 
-    access_to_website("Your GLPI Website")
+    access_to_website("Votre site GLPI")
 
 except:
    logger.error("website unreachable")
@@ -56,7 +69,7 @@ else:
 
 # Le script rentre les identifiant et les mots de passes
 
-chrome.find_element_by_xpath("//input[@id = 'login_name']").send_keys("login")
+chrome.find_element_by_xpath("//input[@id = 'login_name']").send_keys("username")
 chrome.find_element_by_xpath("//input[@id = 'login_password']").send_keys("password")
 
 
@@ -81,7 +94,7 @@ submit_button.click()
 time.sleep(2)
 
 # Permet à l'utilisateur de faire un choix sur son Export
-choix = input("Que voulez vous executer, Ordinateurs ou Téléphones?")
+choix = input("Quel export voulez vous faire?")
 
 #try:
 
@@ -101,9 +114,11 @@ try:
             submit_button = chrome.find_element_by_xpath('//*[@id="show_all_menu"]/table[1]/tbody/tr[6]/td/a')
             submit_button.click()
 
-            #Cliquez sur le bouton export
-            submit_button = chrome.find_element_by_xpath('//input[@name="export"]')
-            submit_button.click()
+
+
+#Cliquez sur le bouton export
+    submit_button = chrome.find_element_by_xpath('//input[@name="export"]')
+    submit_button.click()
 
 
 except Exception as exception:
@@ -114,4 +129,48 @@ else:
     logger.info('Export Successful')
 
 
+
+subject = "An email with attachment from Python"
+body = "This is an email with attachment sent from Python"
+sender_email = "votre email"
+receiver_email = "email du destinataire"
+password = input("Entrez votre mot de passe et appuyez sur entrée:")
+
+# Permet le multipart du mail
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message["Bcc"] = receiver_email  # Recommander pour les mails en masses.
+
+# Ajoute un corps au mail
+message.attach(MIMEText("Tapez,\r\n \r\votre, \r\n \r\n texte"))
+
+filename = r"C:\le\chemin\de\votre\fichier"  # Le fichier
+
+# Open PDF file in binary mode
+with open(filename, "rb") as attachment:
+    # Permet l'ajout du fichier
+    # Permet le téléchargement du fichier en piece
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
+
+# Encode le fichier en caractères  ASCII pour l'envoi du mail
+encoders.encode_base64(part)
+
+# Ajoute un en-tête en tant que paire clé / valeur à la pièce jointe
+part.add_header(
+    "Content-Disposition",
+    f"attachment; filename= {filename}",
+)
+
+# Ajoute une pièce jointe au message et convertir le message en chaîne
+message.attach(part)
+text = message.as_string()
+
+# Connecte au serveur en utilisant un contexte sécurisé et envoyez un e-mail
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, text)
 
